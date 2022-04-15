f83cce445d813d5d121e4f6d9c10d54ab777301d
Lexikus <alexander.bifulco@gmail.com>
2022-04-15 23:22:11 +0200

    feature: started with banlist command

--- src/service.rs
+++ src/service.rs
@@ -3,6 +3,7 @@ use serde_json::{from_str, Value};
 use serenity::model::channel::Message;
 use serenity::client::Context;
 use serenity::framework::standard::{
+    Args,
     CommandResult,
     macros::{
         command,
@@ -13,11 +14,16 @@ use super::NITRADO_TOKEN;
 use super::NITRADO_SERVICE_ID;
 
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Parameter {
+pub struct RestartParameter {
     pub message: String,
-    #[serde(rename = "restart_message ")]
     pub restart_message: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BanParameter {
+    pub category: String,
+    pub key: String,
+    pub value: String
 }
 
 #[group]
@@ -25,7 +31,7 @@ pub struct Parameter {
 pub struct Service;
 
 #[command]
-#[sub_commands(restart, player_count)]
+#[sub_commands(restart, player_count, ban)]
 pub async fn service(_ctx: &Context, _msg: &Message) -> CommandResult {
     Ok(())
 }
@@ -33,7 +39,7 @@ pub async fn service(_ctx: &Context, _msg: &Message) -> CommandResult {
 #[command]
 #[allowed_roles("Admin")]
 pub async fn restart(ctx: &Context, msg: &Message) -> CommandResult {
-    let parameter = Parameter {
+    let parameter = RestartParameter {
         message: "Restart".to_string(),
         restart_message: "Discord bot requested a restart".to_string()
     };
@@ -66,13 +72,48 @@ pub async fn player_count(ctx: &Context, msg: &Message) -> CommandResult {
     if !response.status().is_success() {
         msg.reply(ctx, "Something went wrong.").await?;
         eprintln!("{}", response.text().await?);
-    } else {
-        let text = response.text().await?;
-        let body: Value = from_str(&text)?;
-        let current = body["data"]["gameserver"]["query"]["player_current"].as_i64().unwrap_or(0i64);
-        let max = body["data"]["gameserver"]["query"]["player_max"].as_i64().unwrap_or(0i64);
-        msg.reply(ctx, format!("{current}/{max}")).await?;
+        return Ok(());
     }
+
+    let text = response.text().await?;
+    let body: Value = from_str(&text)?;
+    let current = body["data"]["gameserver"]["query"]["player_current"].as_i64().unwrap_or(0i64);
+    let max = body["data"]["gameserver"]["query"]["player_max"].as_i64().unwrap_or(0i64);
+    msg.reply(ctx, format!("{current}/{max}")).await?;
+
+    Ok(())
+}
+
+// FIXME: this needs to request the gameservers endpoint first,
+// fetch the banlist, and create a new list with windows newlines
+#[command]
+#[allowed_roles("Admin")]
+pub async fn ban(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let identifier = args.rest().to_string();
+    if identifier.is_empty() {
+        msg.reply(ctx, "No identifier provided").await?;
+        return Ok(());
+    }
+
+    let parameter = BanParameter {
+        category: "general".to_string(),
+        key: "bans".to_string(),
+        value: identifier
+    };
+    // let client = reqwest::Client::new()
+    //     .post(format!("https://api.nitrado.net/services/{}/gameservers/settings", *NITRADO_SERVICE_ID))
+    //     .header("Authorization", format!("Bearer {}", *NITRADO_TOKEN))
+    //     .json(&parameter);
+
+    // let response = client.send().await?;
+
+    // if !response.status().is_success() {
+    //     msg.reply(ctx, "Something went wrong.").await?;
+    //     eprintln!("{}", response.text().await?);
+    //     return Ok(());
+    // }
+
+    msg.reply(ctx, format!("Player {} is \"banned\"(not really) now!", parameter.value)).await?;
 
     Ok(())
 }
